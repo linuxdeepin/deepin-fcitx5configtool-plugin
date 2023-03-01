aa01505befb55e8004a267326e643f2ba05410ae
zsien <quezhiyong@uniontech.com>
2023-03-01 16:40:51 +0800

    chore: 更新翻译
    修复添加输入法页面标题未翻译

--- translations/deepin-fcitx5configtool-plugin_hu.ts
+++ translations/deepin-fcitx5configtool-plugin_hu.ts
@@ -1,13 +1,15 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="hu">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
 <context>
     <name>Fcitx5ConfigPlugin</name>
     <message>
-        <location filename="../src/fcitx5configplugin.cpp" line="15"/>
+        <location filename="../src/fcitx5configplugin.cpp" line="28"/>
         <source>Input Methods</source>
         <translation>Beviteli módok</translation>
     </message>
     <message>
-        <location filename="../src/fcitx5configplugin.cpp" line="30"/>
+        <location filename="../src/fcitx5configplugin.cpp" line="43"/>
         <source>Input Method</source>
         <translation>Beviteli mód</translation>
     </message>
@@ -15,32 +17,32 @@
 <context>
     <name>IMSettingWindow</name>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="110"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="179"/>
         <source>Scroll between input methods</source>
         <translation>Görgessen a Beviteli módok között</translation>
     </message>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="112"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="182"/>
         <source>Switch between the current/first input method</source>
         <translation>Váltás az aktuális / első beviteli mód között</translation>
     </message>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="117"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="72"/>
         <source>Manage Input Methods</source>
         <translation>Beviteli módok kezelése</translation>
     </message>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="124"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="166"/>
         <source>Shortcuts</source>
         <translation>Gyorsbillentyűk</translation>
     </message>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="127"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="171"/>
         <source>Restore Defaults</source>
         <translation>Alapértelmezések visszaállítása</translation>
     </message>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="137"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="197"/>
         <source>Advanced Settings</source>
         <translation>Haladó beállítások</translation>
     </message>
@@ -48,13 +50,13 @@
 <context>
     <name>dcc_fcitx_configtool::widgets::FcitxKeyLabelWidget</name>
     <message>
-        <location filename="../src/widgets/keysettingsitem.cpp" line="46"/>
-        <location filename="../src/widgets/keysettingsitem.cpp" line="169"/>
+        <location filename="../src/widgets/keysettingsitem.cpp" line="31"/>
+        <location filename="../src/widgets/keysettingsitem.cpp" line="146"/>
         <source>None</source>
         <translation>Nincs</translation>
     </message>
     <message>
-        <location filename="../src/widgets/keysettingsitem.cpp" line="52"/>
+        <location filename="../src/widgets/keysettingsitem.cpp" line="37"/>
         <source>Enter a new shortcut</source>
         <translation>Adjon meg egy új gyorsbillentyűt</translation>
     </message>
@@ -62,27 +64,31 @@
 <context>
     <name>fcitx::addim::AddIMWindow</name>
     <message>
-        <location filename="../src/addim/widgetslib/addimwindow.cpp" line="34"/>
         <source>Select your language and add input methods</source>
-        <translation>Válassza ki a nyelvet, és adjon hozzá beviteli módokat</translation>
+        <translation type="vanished">Válassza ki a nyelvet, és adjon hozzá beviteli módokat</translation>
     </message>
 </context>
 <context>
     <name>fcitx::addim::IMPage</name>
     <message>
-        <location filename="../src/addim/widgetslib/impage.cpp" line="217"/>
+        <location filename="../src/addim/widgetslib/impage.cpp" line="59"/>
+        <source>Select your language and add input methods</source>
+        <translation type="unfinished">Válassza ki a nyelvet, és adjon hozzá beviteli módokat</translation>
+    </message>
+    <message>
+        <location filename="../src/addim/widgetslib/impage.cpp" line="131"/>
         <source>Find more in App Store</source>
         <translation>További megoldásokat az Áruházban talál</translation>
     </message>
     <message>
-        <location filename="../src/addim/widgetslib/impage.cpp" line="228"/>
+        <location filename="../src/addim/widgetslib/impage.cpp" line="142"/>
         <source>Cancel</source>
         <translation>Mégsem</translation>
     </message>
     <message>
-        <location filename="../src/addim/widgetslib/impage.cpp" line="238"/>
+        <location filename="../src/addim/widgetslib/impage.cpp" line="145"/>
         <source>Add</source>
-        <translation type="unfinished"/>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
