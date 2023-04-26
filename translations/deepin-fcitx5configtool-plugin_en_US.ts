43ae6fdb1bd1d0ae5798db36721b804503c08d98
zsien <quezhiyong@uniontech.com>
2023-04-26 14:24:05 +0800

    chore: load translations in dde-control-center
    load translations in dde-control-center

--- translations/deepin-fcitx5configtool-plugin_en_US.ts
+++ translations/deepin-fcitx5configtool-plugin_en_US.ts
@@ -4,12 +4,16 @@
 <context>
     <name>Fcitx5ConfigPlugin</name>
     <message>
-        <location filename="../src/fcitx5configplugin.cpp" line="28"/>
         <source>Input Methods</source>
-        <translation>Input Methods</translation>
+        <translation type="vanished">Input Methods</translation>
     </message>
     <message>
-        <location filename="../src/fcitx5configplugin.cpp" line="43"/>
+        <location filename="../src/fcitx5configplugin.cpp" line="25"/>
+        <source>deepin-fcitx5configtool-plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/fcitx5configplugin.cpp" line="40"/>
         <source>Input Method</source>
         <translation>Input Method</translation>
     </message>
@@ -17,32 +21,32 @@
 <context>
     <name>IMSettingWindow</name>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="179"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="178"/>
         <source>Scroll between input methods</source>
         <translation>Scroll between input methods</translation>
     </message>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="182"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="181"/>
         <source>Switch between the current/first input method</source>
         <translation>Switch between the current/first input method</translation>
     </message>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="72"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="117"/>
         <source>Manage Input Methods</source>
         <translation>Manage Input Methods</translation>
     </message>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="166"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="165"/>
         <source>Shortcuts</source>
         <translation>Shortcuts</translation>
     </message>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="171"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="170"/>
         <source>Restore Defaults</source>
         <translation>Restore Defaults</translation>
     </message>
     <message>
-        <location filename="../src/window/imsettingwindow.cpp" line="197"/>
+        <location filename="../src/window/imsettingwindow.cpp" line="196"/>
         <source>Advanced Settings</source>
         <translation>Advanced Settings</translation>
     </message>
@@ -84,22 +88,22 @@
 <context>
     <name>fcitx::addim::IMPage</name>
     <message>
-        <location filename="../src/addim/widgetslib/impage.cpp" line="78"/>
+        <location filename="../src/addim/widgetslib/impage.cpp" line="94"/>
         <source>Select your language and add input methods</source>
         <translation type="unfinished">Select your language and add input methods</translation>
     </message>
     <message>
-        <location filename="../src/addim/widgetslib/impage.cpp" line="139"/>
+        <location filename="../src/addim/widgetslib/impage.cpp" line="157"/>
         <source>Find more in App Store</source>
         <translation>Find more in App Store</translation>
     </message>
     <message>
-        <location filename="../src/addim/widgetslib/impage.cpp" line="150"/>
+        <location filename="../src/addim/widgetslib/impage.cpp" line="168"/>
         <source>Cancel</source>
         <translation>Cancel</translation>
     </message>
     <message>
-        <location filename="../src/addim/widgetslib/impage.cpp" line="153"/>
+        <location filename="../src/addim/widgetslib/impage.cpp" line="171"/>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
