464695f9b1ca3ba9e721154ff0154d0f21844d57
Wang Yu <wangyu@uniontech.com>
2024-11-15 16:46:46 +0800

    feat: Add fcitx5 global config page
    - Add Qt6::Qml dependency for QML support
    - Create fcitx5 config proxy and worker classes
    - Implement global config page with dynamic config items
    - Add DetailConfigItem component for different config types
    
    Log: Add fcitx5 global config page

--- src/dcc-fcitx5configtool/operation/fcitx5configtool.cpp
+++ src/dcc-fcitx5configtool/operation/fcitx5configtool.cpp
@@ -2,22 +2,65 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 #include "fcitx5configtool.h"
+#include "private/fcitx5configtool_p.h"
+#include "fcitx5configproxy.h"
+
+#include "dbusprovider.h"
+#include "imconfig.h"
+#include "model.h"
 
 #include "dde-control-center/dccfactory.h"
+
+#include <fcitxqtdbustypes.h>
 
 #include <QCoreApplication>
 #include <QDebug>
 #include <QGuiApplication>
 #include <QtQml/QQmlEngine>
 
-// #include "dccfactory.h"
-
 namespace deepin {
 namespace fcitx5configtool {
-Fcitx5ConfigToolWorker::Fcitx5ConfigToolWorker(QObject *parent)
-    : QObject(parent)
-{
 
+static QString kFcitxConfigGlobalPath = "fcitx://config/global";
+
+Fcitx5ConfigToolWorkerPrivate::Fcitx5ConfigToolWorkerPrivate(Fcitx5ConfigToolWorker *parent)
+    : QObject(parent), q(parent)
+{
+    
+}
+
+void Fcitx5ConfigToolWorkerPrivate::init()
+{
+    dbusProvider = new fcitx::kcm::DBusProvider(this);
+    imConfig = new fcitx::kcm::IMConfig(dbusProvider, fcitx::kcm::IMConfig::Tree, this);
+    configProxy = new Fcitx5ConfigProxy(dbusProvider, kFcitxConfigGlobalPath, this);
+    initConnect();
+}
+
+void Fcitx5ConfigToolWorkerPrivate::initConnect()
+{
+    connect(imConfig, &fcitx::kcm::IMConfig::imListChanged, this, [=]() {
+        qInfo() << "list changed:" << imConfig->currentIMModel()->rowCount();
+    });
+    connect(configProxy, &Fcitx5ConfigProxy::requestConfigFinished, q, &Fcitx5ConfigToolWorker::requestConfigFinished);
+    configProxy->requestConfig(false);
+}
+
+Fcitx5ConfigToolWorker::Fcitx5ConfigToolWorker(QObject *parent)
+    : QObject(parent), d(new Fcitx5ConfigToolWorkerPrivate(this))
+{
+    qmlRegisterType<Fcitx5ConfigProxy>("com.deepin.dcc.fcitx5configtool", 1, 0, "Fcitx5ConfigProxy");
+    QMetaObject::invokeMethod(this, "init", Qt::QueuedConnection);
+}
+
+void Fcitx5ConfigToolWorker::init()
+{
+    d->init();
+}
+
+Fcitx5ConfigProxy *Fcitx5ConfigToolWorker::fcitx5ConfigProxy() const
+{
+    return d->configProxy;
 }
 
 DCC_FACTORY_CLASS(Fcitx5ConfigToolWorker)
