7d09c57076e95ec3ee4249106429cb1a52600a03
Wang Yu <wangyu@uniontech.com>
2025-04-24 19:24:23 +0800

    feat: add fcitx5-helper executable and desktop entry
    Add the fcitx5-helper executable and its desktop entry for better integration.
    
    Log: add fcitx5-helper and update CMakeLists for installation

---
+++ src/fcitx5helper/processmonitor.h
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2024 - 2027 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+#ifndef PROCESSMONITOR_H
+#define PROCESSMONITOR_H
+
+#include <QObject>
+#include <QTimer>
+#include <QString>
+class ProcessMonitor : public QObject 
+{
+    Q_OBJECT
+
+public:
+    explicit ProcessMonitor(QObject *parent = nullptr);
+    void startMonitoring();
+
+    static bool exeCommand(const QString &cmd, const QStringList &args, QString &output, QString &err);
+
+private:
+    QTimer m_timer;
+    QString m_previousProcessID;
+
+    void checkFcitx5Process();
+};
+
+#endif // PROCESSMONITOR_H
