dcc062088a773ec1710609e1173c27bccbee8fa5
dengzhongyuan <dengzhongyuan@uniontech.com>
2025-08-07 17:46:58 +0800

    chore: Add new Lao translation file
    - Add new Lao translation file
    
    Log: Improve language support by adding Lao translation

--- src-old/src/dcc-module/translations/deepin-fcitx5configtool-plugin_lo.ts
+++ src-old/src/dcc-module/translations/deepin-fcitx5configtool-plugin_lo.ts
@@ -3,75 +3,75 @@
     <name>AddIMPage</name>
     <message>
         <source>Select your language and add input methods</source>
-        <translation type="unfinished"/>
+        <translation>ເລືອກພາສາຂອງທ່ານ ແລະ ເພີ່ມວິທີການປ້ອນຂໍ້ມູນ</translation>
     </message>
     <message>
         <source>Find more in App Store</source>
-        <translation type="unfinished"/>
+        <translation>ຊອກຫາເພີ່ມເຕີມໃນຮ້ານອັບ</translation>
     </message>
     <message>
         <source>Cancel</source>
-        <translation type="unfinished"/>
+        <translation>ຍົກເລີກ</translation>
     </message>
     <message>
         <source>Add</source>
-        <translation type="unfinished"/>
+        <translation>ເພີ່ມ</translation>
     </message>
 </context>
 <context>
     <name>Fcitx5ConfigPlugin</name>
     <message>
         <source>deepin-fcitx5configtool-plugin</source>
-        <translation type="unfinished"/>
+        <translation>ສ່ວນເຕີມສຳລັບການຈັດການວິທີການປ້ອນຂໍ້ມູນ</translation>
     </message>
     <message>
         <source>Input Method</source>
-        <translation type="unfinished"/>
+        <translation>ວິທີການປ້ອນຂໍ້ມູນ</translation>
     </message>
 </context>
 <context>
     <name>IMSettingWindow</name>
     <message>
         <source>Manage Input Methods</source>
-        <translation type="unfinished"/>
+        <translation>ຈັດການວິທີການປ້ອນຂໍ້ມູນ</translation>
     </message>
     <message>
         <source>Shortcuts</source>
-        <translation type="unfinished"/>
+        <translation>ສັງລວມ</translation>
     </message>
     <message>
         <source>Restore Defaults</source>
-        <translation type="unfinished"/>
+        <translation>ກັບຄືນຄ່າເລີ່ມຕົ້ນ</translation>
     </message>
     <message>
         <source>Scroll between input methods</source>
-        <translation type="unfinished"/>
+        <translation>ລໍ້ນ້ອຍລະຫວ່າງວິທີການປ້ອນຂໍ້ມູນ</translation>
     </message>
     <message>
         <source>Switch between the current/first input method</source>
-        <translation type="unfinished"/>
+        <translation>ສະແດງລະຫວ່າງວິທີການປ້ອນຂໍ້ມູນປັດຈຸບັນ/ວິທີການປ້ອນຂໍ້ມູນອັນດັບທຳອິດ</translation>
     </message>
     <message>
         <source>Advanced Settings</source>
-        <translation type="unfinished"/>
+        <translation>ການຕັ້ງຄ່າຂັ້ນສູງ</translation>
     </message>
 </context>
 <context>
     <name>LayoutWidget</name>
     <message>
         <source>The current input method has no keyboard layout</source>
-        <translation type="unfinished"/>
+        <translation>ວິທີການປ້ອນຂໍ້ມູນປັດຈຸບັນບໍ່ມີການຈັດຮູບແບບກີບ</translation>
     </message>
     <message>
         <source>Multiple input methods have been selected</source>
-        <translation type="unfinished"/>
+        <translation>ມີການປ້ອນຂໍ້ມູນຫຼາຍຢ່າງທີ່ຖືກເລືອກ</translation>
     </message>
 </context>
 <context>
     <name>dcc_fcitx_configtool::widgets::FcitxKeyLabelWidget</name>
     <message>
         <source>Enter a new shortcut</source>
-        <translation type="unfinished"/>
+        <translation>ປ້ອນສັ້ນໆໃໝ່</translation>
     </message>
 </context>
 </TS>
